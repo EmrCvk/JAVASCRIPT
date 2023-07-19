8582f6ee099a6ef8f1345be0a8fcae3bd28d904c
EmrCvk <cevikemir0@hotmail.com>
2023-07-19 10:35:49 +0300

    added delete button to test without function

--- test/script.js
+++ test/script.js
@@ -1,9 +1,29 @@
-document.getElementById('search-form').addEventListener('submit', function(event) {
-    event.preventDefault(); // Formun otomatik olarak gönderilmesini engeller
+let addList = document.querySelector("#addToListButton")
 
-    var searchQuery = document.getElementById('search-input').value;
-    searchQuery = encodeURIComponent(searchQuery); // URL kodlaması
 
-    var searchResults = document.getElementById('search-results');
-    searchResults.innerHTML = 'Google\'da "' + searchQuery + '" için arama sonuçları gösterilecek.';
-});
+
+
+function addToList() {
+    let list= document.querySelector("#list1");
+    let newListItem=document.createElement("li");
+    
+    var textInput=document.querySelector("#textInput").value;
+    if(textInput <=0)   {
+        alert("0dan büyük bir sayı girin.")
+    }
+    
+    var node = document.createTextNode(textInput);
+
+    newListItem.append(node);
+    list.append(newListItem);
+}
+
+function deleteFromList() {
+    let list= document.querySelector("#list1");
+    let deleteListItem=document.removeChild("li");
+
+    var numberInput=document.querySelector("#numberInput").value
+    var node = document.removeChild(list[deleteListItem -1])
+    
+    console.log(`Deleted item ${numberInput}`)
+}
