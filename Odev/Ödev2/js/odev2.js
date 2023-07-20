d9f40b263a5752f1ba8b8309a841ea9ce34ed867
EmrCvk <cevikemir0@hotmail.com>
2023-07-20 11:19:16 +0300

    completed 2

--- Odev/Ödev2/js/odev2.js
+++ Odev/Ödev2/js/odev2.js
@@ -1,71 +1,34 @@
+document.getElementById("liveToastBtn").addEventListener("click", function () {
+  var task = document.getElementById("task").value.trim();
 
+  if (task === "") {
+    $("#liveToastEmpty").toast("show");
+  } else {
+    var li = document.createElement("li");
+    li.textContent = task;
 
-var list = document.getElementById("list");
-var gorev = document.getElementById("task");
-var ekle = document.getElementById("liveToastBtn");
-var li = document.querySelector("li ")
+    var deleteButton = document.createElement("button");
+    deleteButton.textContent = "X";
+    deleteButton.className = "btn";
+    deleteButton.style.fontWeight = "bold";
 
-var emptyValue = document.querySelector("#liveToastEmpty")
-var addedValue = document.querySelector("#liveToastAdded")
+    li.appendChild(deleteButton);
+    list.appendChild(li);
 
-
-
-
-
-
-ekle.addEventListener("click", function newElement () { //Listeye eleman ekler.
-  
-  if(gorev.value== "") {
-    $(document).ready(function(){ // Input bbos ise liveToastEmty toasterini calistir
-      $('#liveToastEmpty').toast('show');
-    });
+    $("#liveToast").toast("show");
+    document.getElementById("task").value = "";
   }
-
-  else  {
-  var li = document.createElement("li");
-  li.textContent = gorev.value;
-
-
-  var deleteButton = document.createElement("button");
-  deleteButton.textContent = "X";
-  deleteButton.className = "btn";
-  deleteButton.style.fontWeight = "bold";
-
-  li.appendChild(deleteButton);
-  list.appendChild(li);
-
-  $(document).ready(function(){  // Input bos degilse liveToastAdded toasterini calistir.
-    $('#liveToastAdded').toast('show');
-  });
-
-  gorev.value = "";
-        }
 });
 
-
-
-
-
-
-
-
-var list = document.getElementById("list"); //Listeden eleman siler.
-
-list.addEventListener("click", function(event) {
+document.getElementById("list").addEventListener("click", function (event) {
   if (event.target.classList.contains("btn")) {
     var listItem = event.target.parentNode;
     listItem.remove();
   }
 });
 
-
-
-function displayCheckedGroup() { // Listedeki elementi yapıldı olarak işaretler.
-    var elem, i;
-    for (i = 0; i < 5; i++) {
-        elem = document.getElementById("in" + i)
-        if (elem.checked) {
-            alert(elem.nextSibling.nodeValue);
-        }
-    }
-}
+document.getElementById("list").addEventListener("click", function (event) {
+  if (event.target.tagName === "LI") {
+    event.target.classList.toggle("checked");
+  }
+});
